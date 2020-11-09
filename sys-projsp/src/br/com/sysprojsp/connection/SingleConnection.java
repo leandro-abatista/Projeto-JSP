d82d36c59c115f405cf35355eb7d4b25e3909630
Leandro <Leandro@DESKTOP-SHKFPNB>
2020-11-09 09:42:17 -0300

    Criando a classe de conexão com o banco de dados postgreSQL.

---
+++ sys-projsp/src/br/com/sysprojsp/connection/SingleConnection.java
@@ -0,0 +1,50 @@
+package br.com.sysprojsp.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+	
+	private static String bancodedados = "jdbc:postgresql://localhost:5432/sys-projsp?autoReconnect=true";
+	private static String user = "admin";
+	private static String password = "admin";
+	private static Connection connection = null;
+
+	static {
+		conectar();
+	}
+
+	public SingleConnection() {
+		conectar();
+	}
+
+	private static void conectar() {
+		try {
+
+			if (connection == null) {
+
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(bancodedados, user, password);
+				connection.setAutoCommit(false);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Erro ao conectar com o banco de dados!");
+		}
+	}
+
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+	public void fecharConexao() {
+		try {
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Erro ao tentar fechar conexão com o banco de dados!");
+		}
+	}
+
+}
